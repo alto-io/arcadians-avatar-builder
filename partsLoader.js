18aefc0f2c6423ec2b5928b7c2cb9ab11fee4ce8
Marc Anthony Polican <marc@altitude-games.com>
2022-12-19 23:05:30 +0800

    put the file list in a js file instead of loading the directory

--- partsLoader.js
+++ partsLoader.js
@@ -1,6 +1,5 @@
 class PartInfo {
 	matName = "";
-	path = "";
 }
 
 class PartsLoader {
@@ -25,7 +24,7 @@ class PartsLoader {
 
 		var info = this.infoList[key];
 		if (info == null) return;
-		if (!info.matName || !info.path) return;
+		if (!info.matName) return;
 
 		var mat = g_scene.getMaterialByName(info.matName);
 		var tex = this.#loadedTextures.find((x) => x.name == fileName);
@@ -33,7 +32,7 @@ class PartsLoader {
 			mat.albedoTexture = tex;
 		} else {
 			tex = new BABYLON.Texture(
-				info.path + fileName,
+				"arcadian-parts/" + fileName,
 				g_scene,
 				true,
 				false,
@@ -73,44 +72,23 @@ class PartsLoader {
 		}
 	}
 
+    #getFiles(gender, part) {
+        for (var i of g_fileList.List) {
+            if (i.Gender == gender && i.Part == part) {
+                return i.Files;
+            }
+        }
+    }
+
 	#getPartsInfo(isMale) {
 		if (isMale) this.#getMaleParts();
 		else this.#getFemaleParts();
 
+        var gender = isMale ? "Male" : "Female";
+
 		// Get parts collection
 		for (var key in this.infoList) {
-			var path = this.infoList[key].path;
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.open("GET", this.infoList[key].path, false);
-
-			var instance = this;
-			xmlHttp.onload = () => {
-				if (xmlHttp.status != 200) {
-					console.trace("failed to load file list");
-					return;
-				}
-
-				var fileList = xmlHttp.response.split("\n");
-				var list = [];
-				for (var i = 0; i < fileList.length; i++) {
-					var line = fileList[i].toLowerCase();
-					path = path.toLowerCase();
-
-					if (line.includes(".png")) {
-						var end = line.lastIndexOf(".png") + 4;
-						var start = line.lastIndexOf(">", end) + 1;
-
-						if (end < start || start < 0 || end < 0) continue;
-
-						var file = line.substring(start, end);
-						list.push(file);
-					}
-				}
-
-				instance.list[key] = list;
-			};
-
-			xmlHttp.send(null);
+            this.list[key] = this.#getFiles(gender, key);
 		}
 	}
 
@@ -118,35 +96,27 @@ class PartsLoader {
 	#getMaleParts() {
 		var skin = new PartInfo();
 		skin.matName = "mat.mskin";
-		skin.path = "/arcadian-parts/Male/Skin/";
 
 		var eyes = new PartInfo();
 		eyes.matName = "mat.meyes";
-		eyes.path = "/arcadian-parts/Male/Eyes/";
 
 		var head = new PartInfo();
 		head.matName = "mat.mhead";
-		head.path = "/arcadian-parts/Male/Head/";
 
 		var mouth = new PartInfo();
 		mouth.matName = "mat.mmouth";
-		mouth.path = "/arcadian-parts/Male/Mouth/";
 
 		var top = new PartInfo();
 		top.matName = "mat.mtop";
-		top.path = "/arcadian-parts/Male/Top/";
 
 		var leftHand = new PartInfo();
 		leftHand.matName = "mat.mleft";
-		leftHand.path = "/arcadian-parts/Male/Left%20Hand/";
 
 		var rightHand = new PartInfo();
 		rightHand.matName = "mat.mright";
-		rightHand.path = "/arcadian-parts/Male/Right%20Hand/";
 
 		var bot = new PartInfo();
 		bot.matName = "mat.mbot";
-		bot.path = "/arcadian-parts/Male/Bottom/";
 
 		this.infoList["Skin"] = skin;
 		this.infoList["Eyes"] = eyes;
@@ -162,35 +132,27 @@ class PartsLoader {
 	#getFemaleParts() {
 		var skin = new PartInfo();
 		skin.matName = "mat.femskin";
-		skin.path = "/arcadian-parts/Female/Skin/";
 
 		var eyes = new PartInfo();
 		eyes.matName = "mat.femeyes";
-		eyes.path = "/arcadian-parts/Female/Eyes/";
 
 		var head = new PartInfo();
 		head.matName = "mat.femhead";
-		head.path = "/arcadian-parts/Female/Head/";
 
 		var mouth = new PartInfo();
 		mouth.matName = "mat.femmouth";
-		mouth.path = "/arcadian-parts/Female/Mouth/";
 
 		var top = new PartInfo();
 		top.matName = "mat.femtop";
-		top.path = "/arcadian-parts/Female/Top/";
 
 		var leftHand = new PartInfo();
 		leftHand.matName = "mat.femleft";
-		leftHand.path = "/arcadian-parts/Female/Left%20Hand/";
 
 		var rightHand = new PartInfo();
 		rightHand.matName = "mat.femright";
-		rightHand.path = "/arcadian-parts/Female/Right%20Hand/";
 
 		var bot = new PartInfo();
 		bot.matName = "mat.fembottom";
-		bot.path = "/arcadian-parts/Female/Bottom/";
 
 		this.infoList["Skin"] = skin;
 		this.infoList["Eyes"] = eyes;
