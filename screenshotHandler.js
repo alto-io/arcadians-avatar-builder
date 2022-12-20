5cdb7c6e4c8850e460f5d114cd79a52c6f9944ca
Kevin Cosgafa <kevin@altitude-games.com>
2022-12-20 18:21:47 +0800

    Change screenshotHandler to go to specific frame in the animation, for more consistent output

--- screenshotHandler.js
+++ screenshotHandler.js
@@ -2,62 +2,55 @@ class ScreenshotHandler {
 	screenshotData = [];
 
 	startScreenshotsCr = function* (engine, scene, animationGroup) {
+		let config = g_config.spritesheet;
+		this.screenshotData.length = 0;
+
 		let prevBgColor = scene.clearColor;
 		scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
-		this.screenshotData.length = 0;
+		// 'to'' is the last frame of the currently playing animation
+		// as in 'from -> to'
+		let totalFrames = animationGroup.to;
 
-		let totalFrames = this.getTotalFramesOfAnim(engine, animationGroup);
 		console.log("Total animation length: " + totalFrames + " frames.");
-
 		// skip some frames, so we don't have to take all 60 frames of a 1 sec animation.
-		// at 0, don't skip any.
-		let skipInterval = 3; // 3 is a good value at 144fps. need to have this adapt to diff framerates
-		console.log("Current Framerate: " + engine.getFps());
+		let skipInterval = config.framesToSkip;
 		console.log(
 			"Skipping " + skipInterval + " frame/s after every screenshot..."
 		);
 
-		let skipCounter = 0;
+		let skipCounter = skipInterval;
 
-		// wait for the anim to reset
-		animationGroup.reset();
-		yield;
-		yield;
-
-		let firstFrame = totalFrames;
-
-		while (totalFrames >= 0) {
-			// take screenshot of the first and last frame
-			// totalFrames may not be a whole number
-			let forceScreenshot = totalFrames == firstFrame || totalFrames < 1;
+		for (let i = 0; i < totalFrames; i++) {
 			let skipFrame = skipInterval > 0 && skipCounter < skipInterval;
 
-			if (forceScreenshot || !skipFrame) {
-				skipCounter = 0;
-
-				let config = g_config.spritesheet;
-				BABYLON.Tools.CreateScreenshotUsingRenderTarget(
-					engine,
-					g_camera,
-					{
-						height: config.sizeX,
-						width: config.sizeY,
-						precision: 1,
-					},
-					this.screenshotSuccess,
-					"image/png",
-					8,
-					false,
-					"screenshot.png"
-				);
-			} else {
+			if (skipFrame) {
 				skipCounter++;
+				continue;
 			}
 
-			totalFrames--;
+			skipCounter = 0;
+
+			// wait for the frame to load
+			animationGroup.goToFrame(i);
 			yield;
+
+			BABYLON.Tools.CreateScreenshotUsingRenderTarget(
+				engine,
+				g_camera,
+				{
+					height: config.sizeX,
+					width: config.sizeY,
+					precision: 1,
+				},
+				this.screenshotSuccess,
+				"image/png",
+				8,
+				false,
+				"screenshot.png"
+			);
 		}
+
 		console.log("Total sprite count: " + this.screenshotData.length);
 		if (this.screenshotData.length > 0) this.zipAndSaveScreenshots();
 		else console.error("Screenshots failed! No screenshots were taken.");
@@ -105,25 +98,5 @@ class ScreenshotHandler {
 		zip.generateAsync({ type: "blob" }).then(function (content) {
 			saveAs(content, zipName);
 		});
-	}
-
-	getTotalFramesOfAnim(engine, animationGroup) {
-		// 'to'' is the last frame of the currently playing animation
-		// as in 'from -> to'
-		let totalAnimFrames = animationGroup.to;
-		//console.log("totalAnimFrames: " + totalAnimFrames);
-
-		// my guess is the arcadian was animated at 60fps initially but idk
-		let intendedFps = 60;
-		//console.log("intendedFps: " + intendedFps);
-
-		// match to fps
-		// the anim is set to match the current frame rate
-		let currFps = engine.getFps();
-		//console.log("currFps: " + currFps);
-
-		let estimatedFrames = (totalAnimFrames / intendedFps) * currFps;
-
-		return estimatedFrames;
 	}
 }
