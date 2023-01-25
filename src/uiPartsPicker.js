9b824c134614716bb9dbbe7553ab70efee50f56b
josephaltitude <joseph@altitude-games.com>
2023-01-25 13:05:47 +0800

    Added linear search function for parts picker UI

--- src/uiPartsPicker.js
+++ src/uiPartsPicker.js
@@ -2,6 +2,22 @@ window.onload = function () {
 	initUI();
 };
 
+// Search List sample format
+var searchList = {
+	Parts: [
+		{
+			Name: "Bottom",
+			Files: [
+				{
+					Name: "Alien-Queen-Bottom",
+					Path: "v1/arcadian-parts/Female/Bottom/Alien-Queen-Bottom.png",
+				},
+			],
+		},
+	],
+}
+var currId = null;
+
 async function initUI() {
 	await waitForLoading();
 
@@ -94,4 +110,74 @@ function initParts(id) {
 
 	// Default to the first material
 	showPartsList(id, avatar.materials[0].name);
+
+	currId = id;
+}
+
+function linearSearch() {
+	if (g_fileList == null) return;
+	if (currId == null);
+
+	// Get the parts from current avatar id
+	var list = g_fileList.find((x) => x.Gender == currId);
+	if (list == null) return;
+
+	var text = document.getElementById("search-text").value;
+	if (text == "") return;
+
+	// Cleanup search list
+	searchList.Parts = [];
+
+	// Add searched parts to list
+	for (let part of list.Parts) {
+		let searchedParts = [];
+		for (let file of part.Files) {
+				if (file.Name.toLowerCase().includes(text.toLowerCase())) {
+				searchedParts.push(file);
+			}
+		}
+
+		if (searchedParts.length > 0) {
+			part.Files = searchedParts;
+			searchList.Parts.push(part);
+		}
+	}
+
+	var div = document.getElementById("list-pick");
+	if (div == null) return;
+	div.innerHTML = "";
+
+	var header = document.createElement("H2");
+	var text = document.createTextNode(text);
+	header.appendChild(text);
+	div.appendChild(header);
+	
+	for(var part of searchList.Parts) {
+		var matName = part.Name;
+
+		for (var i of part.Files) {
+			var group = document.createElement("div");
+			group.id = "button-group";
+	
+			var text = document.createElement("p");
+			text.innerText = i.Name;
+	
+			var button = document.createElement("button");
+			button.innerText = i.Name;
+			button.innerHTML = `<button class="img-size"><img src='${i.Path}' /></button>`;
+			button.setAttribute(
+				"onClick",
+				`replaceParts('${matName}', '${i.Path}')`
+			);
+	
+			// Add button to group
+			group.appendChild(button);
+	
+			// Add text to group;
+			group.appendChild(text);
+	
+			// Add group to grid div
+			div.appendChild(group);
+		}
+	}
 }
