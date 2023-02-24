2e897bdf4c43d2218c58de5162f3cafbada9c5e0
Paul Gadi <paulgadi@gmail.com>
2023-02-24 13:27:14 -0500

    re-style upload button

--- components/OraViewerComponent.jsx
+++ components/OraViewerComponent.jsx
@@ -62,19 +62,17 @@ export default ({ onCanvasReady, ...rest }) => {
     return (
     <>
         <div className="flex w-full gap-2 items-center justify-center">
-            <p className={`font-bold text-[#AA54FF]`}>
-                {oraFileName}
-            </p>
             <button 
                 onClick = {handleFileUploadClick}
                 className={
                     "bg-fuchsia-500 hover:bg-fuchsia-400 text-white font-bold py-2 px-4 border-b-4 border-fuchsia-700 hover:border-fuchsia-500 rounded inline-flex"
                 }
             >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <span> {oraFileName}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
                 </svg>
-                <span>Upload</span>
+
             </button>
             <input
             type="file"
@@ -136,7 +134,7 @@ export default ({ onCanvasReady, ...rest }) => {
                     <></> 
                     : 
                     <p className={`font-bold text-[#AA54FF]`}>
-                        👆 Select a category 👆                         
+                        👆 Select a part category 👆                         
                     </p>
                 }        
             </div>        
